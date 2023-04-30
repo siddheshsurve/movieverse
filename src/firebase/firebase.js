5851068d45b6d59f13ee51bfa6bda0e4130fe8d8
Siddhesh Surve <sysurve24@gmail.com>
2023-04-30 11:54:30 +0000

    basic firebase setup to firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFireStore, collection } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFireStore(app);
+export const moviesRef = collection(db, "movies");
+
+export default app;
