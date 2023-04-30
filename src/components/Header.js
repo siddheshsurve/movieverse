f059632d099500eb121cf1224d4f38b87401af76
Siddhesh Surve <sysurve24@gmail.com>
2023-04-30 04:47:57 +0000

    css added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <div className='text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500'>
         <span>Movie<span className='text-white'>Verse</span></span> 
-        <h1 className='text-lg text-white flex items-center cursor-pointer'>
+        <h1 className='text-lg flex items-center cursor-pointer'>
             <Button className='text-white'>
-                <AddIcon className='mr-1' color='inherit' /> <span></span> Add New
+                <AddIcon className='mr-1' color='secondary' /> <span className='text-white'>Add New</span>
             </Button> 
         </h1>
     </div>
