ae79180bca2910dd78d88717c773f74a0243f0d0
Siddhesh Surve <sysurve24@gmail.com>
2023-04-30 11:34:19 +0000

    firebase installation

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDvdPHICgSX38g-FeR-O5GB-htGzSvCiw",
+  authDomain: "movieverse-e4a55.firebaseapp.com",
+  projectId: "movieverse-e4a55",
+  storageBucket: "movieverse-e4a55.appspot.com",
+  messagingSenderId: "47769681168",
+  appId: "1:47769681168:web:d6c4f239ac65bf57eef995"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
