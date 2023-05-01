a82357e9fe8be795834deac8b864482e163fe2eb
Siddhesh Surve <sysurve24@gmail.com>
2023-05-01 14:06:58 +0000

    created login, signup+added global state to app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import Cards from "./components/Cards";
 import AddMovie from "./components/AddMovie";
 import Detail from "./components/Detail"
 import { Route, Routes } from "react-router-dom";
+import { createContext, useState } from "react";
+
+const Appstate = createContext();
 
 function App() {
+
+  const [login, setLogin] = useState(false);
+  const [userName, setUserName] = useState("");
+
   return (
-    <div className="App relative">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Cards/>} ></Route>
-        <Route path="/addmovie" element={<AddMovie/>}></Route>
-        <Route path="/detail/:id" element={<Detail />}></Route>
-      </Routes>
-    </div>
+    <Appstate.Provider value={{login, userName}}>
+      <div className="App relative">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Cards/>} ></Route>
+          <Route path="/addmovie" element={<AddMovie/>}></Route>
+          <Route path="/detail/:id" element={<Detail />}></Route>
+        </Routes>
+      </div>
+    </Appstate.Provider>
   );
 }
 
