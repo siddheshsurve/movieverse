c66450564c4afbd4d3017d068aec2cbf1f7a6320
Siddhesh Surve <sysurve24@gmail.com>
2023-05-01 16:48:41 +0000

    added routes to login and signup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Cards from "./components/Cards";
 import AddMovie from "./components/AddMovie";
 import Detail from "./components/Detail"
 import { Route, Routes } from "react-router-dom";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import Login from "./components/Login"
+import Signup from "./components/Signup"
 
 const Appstate = createContext();
 
@@ -12,14 +14,20 @@ function App() {
   const [login, setLogin] = useState(false);
   const [userName, setUserName] = useState("");
 
+  useEffect(() => {
+
+  })
+
   return (
-    <Appstate.Provider value={{login, userName}}>
+    <Appstate.Provider value={{login, userName, setLogin, setUserName}}>
       <div className="App relative">
         <Header />
         <Routes>
           <Route path="/" element={<Cards/>} ></Route>
           <Route path="/addmovie" element={<AddMovie/>}></Route>
           <Route path="/detail/:id" element={<Detail />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </div>
     </Appstate.Provider>
@@ -27,3 +35,4 @@ function App() {
 }
 
 export default App;
+export { Appstate };
